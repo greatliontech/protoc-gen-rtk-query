32cb686ef22e28722932f6aba60c66f8e0226aee
Nikolas Sepos <nikolas.sepos@gmail.com>
2022-08-05 21:41:58 +0300

    add providesList function to ts lib

--- lib/index.ts
+++ lib/index.ts
@@ -37,3 +37,16 @@ export const grpcBaseQuery =
       }
     }
 
+// https://redux-toolkit.js.org/rtk-query/usage/automated-refetching#abstracting-common-providesinvalidates-usage
+export function providesList<R extends { id: string | number }[], T extends string>(
+  resultsWithIds: R | undefined,
+  tagType: T
+) {
+  return resultsWithIds
+    ? [
+      { type: tagType, id: 'LIST' },
+      ...resultsWithIds.map(({ id }) => ({ type: tagType, id })),
+    ]
+    : [{ type: tagType, id: 'LIST' }]
+}
+
